e86d2ceaa6428acda8d2257aadfb2f70f2000786
Masaya Kataoka <ms.kataoka@gmail.com>
2021-08-14 21:39:15 +0900

    add error check function
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- dist/setup_tool.js
+++ dist/setup_tool.js
@@ -5,7 +5,7 @@ var SetupTool = /** @class */ (function () {
     function SetupTool(template_path, parameters) {
         this.template_path_ = template_path;
         this.parameters_ = parameters;
-        this.parameters_json = JSON.parse(this.parameters_);
+        this.replaceAll(JSON.parse(this.parameters_));
     }
     Object.defineProperty(SetupTool.prototype, "template_path", {
         get: function () {
@@ -14,7 +14,20 @@ var SetupTool = /** @class */ (function () {
         enumerable: false,
         configurable: true
     });
-    SetupTool.prototype.replace = function (key, value) {
+    SetupTool.prototype.replace = function (key, value) { };
+    SetupTool.prototype.replaceAll = function (json) {
+        for (var _i = 0, _a = Object.entries(json); _i < _a.length; _i++) {
+            var _b = _a[_i], key = _b[0], value = _b[1];
+            console.log(key + ": " + value);
+            if (typeof key != "string") {
+                throw new Error("key of the parameter should be string");
+            }
+            if (typeof value != "string" &&
+                typeof value != "boolean" &&
+                typeof value != "number") {
+                throw new Error("value of the parameter should be string, boolean, number");
+            }
+        }
     };
     return SetupTool;
 }());
--- src/setup_tool.ts
+++ src/setup_tool.ts
@@ -1,8 +1,9 @@
 import * as fs from "fs";
+import { type } from "os";
+import { stringify } from "querystring";
 
 export class SetupTool {
   private template_path_: string;
-  private parameters_json : JSON;
 
   public get template_path(): string {
     return this.template_path_;
@@ -10,13 +11,29 @@ export class SetupTool {
 
   private parameters_: string;
 
-  private replace(key : string, value: string) {
-    
+  private replace(key: string, value: string) {}
+
+  private replaceAll(json: JSON) {
+    for (const [key, value] of Object.entries(json)) {
+      console.log(`${key}: ${value}`);
+      if (typeof key != "string") {
+        throw new Error("key of the parameter should be string");
+      }
+      if (
+        typeof value != "string" &&
+        typeof value != "boolean" &&
+        typeof value != "number"
+      ) {
+        throw new Error(
+          "value of the parameter should be string, boolean, number"
+        );
+      }
+    }
   }
 
   constructor(template_path: string, parameters: string) {
     this.template_path_ = template_path;
     this.parameters_ = parameters;
-    this.parameters_json = JSON.parse(this.parameters_);
+    this.replaceAll(JSON.parse(this.parameters_));
   }
 }
