bed0e52a5013f7283c1729773a1604f778ad4f98
Masaya Kataoka <ms.kataoka@gmail.com>
2021-08-14 18:00:45 +0900

    modify parameter
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- src/setup_workflow_action.test.ts
+++ src/setup_workflow_action.test.ts
@@ -3,7 +3,7 @@ import { SetupTool } from "./setup_tool";
 test("read_repos", () => {
   const updater = new SetupTool(
     "./src/example_workflow.yaml",
-    "{ who-to-greet : octcat }"
+    "{ \"who-to-greet\" : \"octcat\" }"
   );
   expect(updater).toBeDefined();
 });
